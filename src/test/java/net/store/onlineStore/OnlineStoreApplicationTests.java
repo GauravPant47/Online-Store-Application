4fab3927ae8e1a871d0a55d867ede72c51437d65
Gaurav <gaurav556pant@gmail.com>
2023-03-18 01:03:39 +0530

    adding @SpringBootTest(classes = runnableclassname.class) in shide OnlineStoreApplicationTests.java

--- src/test/java/net/store/onlineStore/OnlineStoreApplicationTests.java
+++ src/test/java/net/store/onlineStore/OnlineStoreApplicationTests.java
@@ -3,7 +3,7 @@ package net.store.onlineStore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = OnlineStoreApplication.class)
 class OnlineStoreApplicationTests {
 
 	@Test
